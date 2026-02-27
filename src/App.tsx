90f1d30dfebf0bcdb277efa8acc53b22d302db54
Ismail Cayden <ismailmaho007@gmail.com>
2026-02-27 10:47:49 +0200

    Implement shared card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getWeather } from "./api";
+import Card from "./components/cards/Card";
 
 function App() {
   const { data } = useQuery({
@@ -7,7 +8,19 @@ function App() {
     queryFn: () => getWeather({ lat: 10, lon: 25 }),
   });
 
-  return <>{JSON.stringify(data)}</>;
+  return (
+    <div className="flex flex-col gap-8">
+      <Card title="Current Weather">
+        {JSON.stringify(data?.current).slice(0, 100)}
+      </Card>
+      <Card title="Hourly Forecast (48 Hours)">
+        {JSON.stringify(data?.hourly).slice(0, 100)}
+      </Card>
+      <Card title="Daily Forecast">
+        {JSON.stringify(data?.daily).slice(0, 100)}
+      </Card>
+    </div>
+  );
 }
 
 export default App;
