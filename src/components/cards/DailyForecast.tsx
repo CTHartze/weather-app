4db53cebfc7fda91592723ea18afda1353efa75c
Ismail Cayden <ismailmaho007@gmail.com>
2026-03-12 20:37:57 +0200

    Enhance DailyForecast component to display weather date and icon

--- src/components/cards/DailyForecast.tsx
+++ src/components/cards/DailyForecast.tsx
@@ -13,7 +13,14 @@ export default function DailyForecast({}: Props) {
     <Card title="Daily Forecast">
       <div className="flex flex-col gap-4">
         {data?.daily.map((day) => (
-          <div key={day.dt}></div>
+          <div key={day.dt} className="flex justify-between">
+            <p>DATE</p>
+            <img
+              className="size-8"
+              src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+              alt="Weather Icon"
+            />
+          </div>
         ))}
       </div>
     </Card>
