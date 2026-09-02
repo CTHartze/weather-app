5536917ea6fd30bad778e79120476e98ff25fddc
Ismail Cayden <ismailmaho007@gmail.com>
2026-09-02 16:19:16 +0200

    Add location dropdown and geocoding functionality
    - Introduced LocationDropdown component for selecting cities.
    - Integrated geocoding API to fetch coordinates based on selected location.
    - Enhanced Map component to pan to selected coordinates.
    - Updated styles and themes with new color variables and font sources using shadcn's Radix UI.
    - Added new select component

---
+++ src/components/dropdowns/LocationDropdown.tsx
@@ -0,0 +1,45 @@
+import type { Dispatch, SetStateAction } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+type Props = {
+  location: string;
+  setLocation: Dispatch<SetStateAction<string>>;
+};
+
+export default function LocationDropdown({ location, setLocation }: Props) {
+  const locations = [
+    "New York",
+    "London",
+    "Paris",
+    "Tokyo",
+    "Los Angeles",
+    "Dubai",
+    "Singapore",
+    "Sydney",
+    "Berlin",
+    "Rome",
+    "Barcelona",
+    "Amsterdam",
+  ];
+
+  return (
+    <Select value={location} onValueChange={(value) => setLocation(value)}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Cities" />
+      </SelectTrigger>
+      <SelectContent className="z-1001">
+        {locations.map((city) => (
+          <SelectItem key={city} value={city}>
+            {city}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}
